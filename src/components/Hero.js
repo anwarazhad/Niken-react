4c5bffdddad6a15835092ae57e519efa869f7632
susshi <anwarazhad00@gmail.com>
2023-05-05 00:05:02 +0800

    Gradient Color

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,19 +1,41 @@
 import "./Hero.css";
+import Granim from "granim";
+import { useEffect } from "react";
 
 function Hero(props) {
+
+    useEffect(() => {
+        new Granim({
+            element: "#logo-canvas",
+            direction: "diagonal",
+            opacity: [1, 1],
+            states: {
+                "default-state": {
+                    gradients: [
+                        ["#573c78", "#384569"],
+                        ["#bdc3c7", "#2c3e50"],
+                        ["#7b4397", "#dc2430"],
+                        ["#ffd89b", "#19547b"],
+                    ],
+                    transitionSpeed: 5000,
+                },
+            },
+        });
+    }, []);
+
     return (
         <>
-            <div className={props.cName}>
-                <img className="home-bg" src={props.heroImg} alt="home-bg" />
+            <div className="bloc-logo">
+                <canvas id="logo-canvas" />
+            </div>
 
-                <div className="hero-text">
-                    <p>{props.text}</p>
-                    <h1>{props.title}</h1>
-                    <a href={props.url} className={props.btnClass}>
-                        {props.buttonText}
-                    </a>
-                </div>
-            </div >
+            <div className="hero-text">
+                <h1>{props.text}</h1>
+                <p>{props.title}</p>
+                <a href={props.url} className={props.btnClass}>
+                    {props.buttonText}
+                </a>
+            </div>
         </>
     );
 }
