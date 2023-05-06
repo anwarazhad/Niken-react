42a6a99566538a58bcbabb54ebba08a539abd5fb
susshi <anwarazhad00@gmail.com>
2023-05-07 00:42:07 +0800

    Make about page

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -10,7 +10,7 @@ function Home() {
                 heroImg="https://images.unsplash.com/photo-1584731830441-d485c50e0ac7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
                 text="INNOVATIVE MIND"
                 title="CHALLENGE THE IMPOSSIBLE"
-                btnText="Explore More"
+                btnText="Explore"
                 url="/"
                 btnClass="show"
             />
