dd38664463c029db45f5fde99a582ecbe42f7ede
susshi <anwarazhad00@gmail.com>
2023-05-07 00:36:13 +0800

    Add new background image and make about page

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,40 +1,23 @@
 import "./Hero.css";
-import Granim from "granim";
-import { useEffect } from "react";
-import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown';
 
 function Hero(props) {
 
-    useEffect(() => {
-        new Granim({
-            element: "#logo-canvas",
-            direction: "diagonal",
-            opacity: [1, 1],
-            states: {
-                "default-state": {
-                    gradients: [
-                        ["#573c78", "#384569"],
-                        // ["#bdc3c7", "#2c3e50"],
-                        // ["#7b4397", "#dc2430"],
-                        // ["#ffd89b", "#19547b"],
-                    ],
-                    transitionSpeed: 5000,
-                },
-            },
-        });
-    }, []);
-
     return (
         <>
-            <div className="bloc-logo">
-                <canvas id="logo-canvas" />
-            </div>
 
-            <div className="hero-text">
-                <h1>{props.text}</h1>
-                <p>{props.title}</p>
-                <KeyboardDoubleArrowDownIcon href={props.url} className={props.btnClass} />
-            </div >
+            <div className={props.cName}>
+                <img alt="HerpImg" src={props.heroImg} />
+
+
+                <div className="hero-text">
+                    <h1>{props.text}</h1>
+                    <p>{props.title}</p>
+                    <a href={props.url} className={props.btnClass}>
+                        {props.btnText}
+                    </a>
+                </div >
+
+            </div>
         </>
     );
 }
--- src/routes/About.js
+++ src/routes/About.js
@@ -1,7 +1,19 @@
+import Hero from "../components/Hero";
+import Navbar from "../components/Navbar"
+
 function About() {
     return (
         <>
-            <h1>This is About</h1>
+            <Navbar />
+            <Hero
+                cName="hero"
+                heroImg="https://images.unsplash.com/photo-1584731830441-d485c50e0ac7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                text="INNOVATIVE MIND"
+                title="CHALLENGE THE IMPOSSIBLE"
+                btnText="Explore More"
+                url="/"
+                btnClass="show"
+            />
         </>
     )
 }
